9fdb1c0405d2be507ed306ec76d653e37448a038
fonchu <fonchu.e.venyuy@gmail.com>
2024-11-21 15:45:24 +0100

    updates

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,9 +1,9 @@
 import { defineStorage } from "@aws-amplify/backend";
-import {paymentWebhook} from '../funtions/paymentWebhook/resource'
+// import {paymentWebhook} from '../funtions/paymentWebhook/resource'
 
 export const storage = defineStorage({
   name: 'songStorage',
-  access: (allow) => ({
-    'songs/*': [allow.resource(paymentWebhook).to(['read'])]
-  })
+  // access: (allow) => ({
+  //   'songs/*': [allow.resource(paymentWebhook).to(['read'])]
+  // })
 })
