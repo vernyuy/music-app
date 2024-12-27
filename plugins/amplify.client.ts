686be59e5c2f1b13b5e91463937f8bc9bd1f2534
Kongbi-Zion <kongbizion3@gmail.com>
2024-12-27 11:59:10 +0100

    resolved deploment issue

--- plugins/amplify.client.ts
+++ plugins/amplify.client.ts
@@ -1,7 +1,7 @@
 import { Amplify } from "aws-amplify";
-import config from "@/src/amplify_outputs.json";
+import config from "@/amplify_outputs.json";
 
 // eslint-disable-next-line no-undef
 export default defineNuxtPlugin(() => {
-  Amplify.configure(config, { ssr: true });
+  Amplify.configure(config);
 });
