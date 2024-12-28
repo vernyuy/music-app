028a2705681675bba32635ac3818c28888680619
Kongbi-Zion <kongbizion3@gmail.com>
2024-12-28 06:53:39 +0100

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
-
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
