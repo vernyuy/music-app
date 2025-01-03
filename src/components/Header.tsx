4345172cd359b0be44b4798e3e3ec3a48efa2cf1
Kongbi-Zion <kongbizion3@gmail.com>
2025-01-03 03:04:25 +0100

    resolved deployment issues

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Logo from "../assets/logo.png";
 import { fetchAuthSession, signOut } from "aws-amplify/auth";
 import { Hub } from "aws-amplify/utils";
